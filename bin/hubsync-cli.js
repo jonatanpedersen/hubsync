948e8a6a524506105de6b5045c60511293120644
Jonatan Pedersen <jp@jonatanpedersen.com>
2018-03-15 00:36:14 +0100

    adds includes filter

--- bin/hubsync-cli.js
+++ bin/hubsync-cli.js
@@ -7,6 +7,7 @@ const optionDefinitions = [
 	{ name: 'token', alias: 't', type: String, defaultValue: process.env.HUBSYNC_TOKEN, description: 'Personal access token for GitHub. Defaults to $HUBSYNC_TOKEN.' },
 	{ name: 'concurrency', alias: 'c', type: Number, defaultValue: 10, description: 'Concurrent child processes. Defaults to 10.' },
 	{ name: 'cwd', alias: 'd', type: String, defaultValue: process.cwd(), description: 'Working directory. Defaults to current working directory.' },
+	{ name: 'include', type: String, defaultOption: true, defaultValue: '*/*', description: 'Include owner/repo filter.' },
 	{ name: 'help', alias: 'h', type: Boolean, description: 'Print this usage guide.' }
 ];
 
